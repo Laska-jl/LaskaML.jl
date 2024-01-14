462b33c5ff06cab7018391e86ba2c333c34ae0a6
Eliassj <elias.sjolin@gmail.com>
2024-01-14 16:56:37 +0100

    1.0.1 docfix

--- src/hh.jl
+++ src/hh.jl
@@ -3,8 +3,15 @@
 """
     ab_generalized(V, p)
 
-The generalized form af α and β.
+The generalized form of α and β.
 
+```
+function ab_generalized(V, p)
+    A, B, C, D, F, H = p
+    (A + B * V) / (C + H * exp((V + D) / F))
+end
+
+```
 
 """
 function ab_generalized(V, p)
