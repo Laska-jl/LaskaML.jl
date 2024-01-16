3110759dfaffcd78cb730687fa830deb8a4036d4
Eliassj <elias.sjolin@gmail.com>
2024-01-16 23:09:03 +0100

    structfunkar

--- src/hh.jl
+++ src/hh.jl
@@ -1,7 +1,8 @@
 
 module hh
 
-
+const FunctionOrNothing = Union{Function, Nothing}
+const NumberOrNothing = Union{Number, Nothing}
 
 """
     ab_generalized(V, p)
@@ -20,6 +21,50 @@ end
 function ab_generalized(V, p)
     A, B, C, D, F, H = p
     (A + B * V) / (C + H * exp((V + D) / F))
+end
+
+"""
+    struct HHChannel{T <: FunctionOrNothing,U <: NumberOrNothing, V <: Number}
+        g::V
+        α_m::T
+        β_m::T
+        p::U
+        α_h::T
+        β_h::T
+        q::U
+    end
+
+General struct for a channel in the Hodgkin-Huxley model.
+
+The general expression of the channel is:
+
+```math
+I_x = \bar{g}_xm^p_xh^q_x(V_mV_x)
+```
+
+The gating variables ``m`` and ``h`` are are given by `α_m`, `β_m`, `α_h` and `β_h` respectively in the following way:
+
+```math
+\begin{aligned}
+\frac{dm}{dt} = \alpha(V_M)(1-m) - \beta(V_M)m \\
+\frac{dh}{dt} = \alpha(V_M)(1-h) - \beta(V_M)h
+\end{aligned}
+```
+
+"""
+struct HHChannel{T <: FunctionOrNothing,U <: NumberOrNothing, V <: Number}
+    g::V  # Max conductance
+    α_m::T # Alpha for gating variable m
+    β_m::T # Beta for gating variable m
+    p::U # Exponent of gating variable m
+    α_h::T # Alpha for gating variable n
+    β_h::T # Beta for garing variable n
+    q::U # Exponent for gating variable h
+end
+
+
+function hhchannel(g, α_m=nothing, β_m=nothing, p=nothing, α_h=nothing, β_h=nothing, q=nothing)
+    HHChannel(g, α_m, β_m, p, α_h, β_h, q)
 end
 
 end
