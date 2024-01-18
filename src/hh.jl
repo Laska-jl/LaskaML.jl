53e466f544ff2122f2d6787c16f7cd467649a32b
Eliassj <elias.sjolin@gmail.com>
2024-01-18 08:24:44 +0100

    premerge

--- src/hh.jl
+++ src/hh.jl
@@ -23,7 +23,7 @@ function ab_generalized(V, p)
     (A + B * V) / (C + H * exp((V + D) / F))
 end
 
-"""
+@doc raw"""
     struct HHChannel{T <: FunctionOrNothing,U <: NumberOrNothing, V <: Number}
         g::V
         Vsteady::V
@@ -35,15 +35,13 @@ end
         q::U
     end
 
-General struct for a channel in the Hodgkin-Huxley model.
-
-The general expression of the channel is:
+General struct for a channel in the Hodgkin-Huxley model in the form:
 
 ```math
 I_x = \bar{g}_xm^p_xh^q_x(V_mV_x)
 ```
 
-The gating variables ``m`` and ``h`` are are given by `α_m`, `β_m`, `α_h` and `β_h` respectively in the following way:
+The gating variables ``m`` and ``h`` are are given by `α_m`, `β_m`, `α_h` and `β_h` respectively in the following wa
 
 ```math
 \begin{aligned}
@@ -53,15 +51,16 @@ The gating variables ``m`` and ``h`` are are given by `α_m`, `β_m`, `α_h` and
 ```
 
 """
-struct HHChannel{T <: FunctionOrNothing,U <: NumberOrNothing, V <: Number}
-    g::V  # Max conductance
-    Vsteady::V # Steady-state voltage
-    α_m::T # Alpha for gating variable m
-    β_m::T # Beta for gating variable m
-    p::U # Exponent of gating variable m
-    α_h::T # Alpha for gating variable n
-    β_h::T # Beta for garing variable n
-    q::U # Exponent for gating variable h
+struct HHChannel{T<:Number, U<:Number}
+    identifier::String
+    g::U  # Max conductance
+    Vsteady::Number # Ion specific reversal potential
+    α_m::FunctionOrNothing # Alpha for gating variable m
+    β_m::FunctionOrNothing # Beta for gating variable m
+    p::T # Exponent of gating variable m
+    α_h::FunctionOrNothing # Alpha for gating variable h
+    β_h::FunctionOrNothing # Beta for garing variable h
+    q::T # Exponent for gating variable h
 end
 
 """
@@ -70,8 +69,39 @@ end
 Outer constructor of the HHChannel. Mostly convenient for *leaky channels* and similar without gating variables.
 
 """
-function hhchannel(g, Vsteady, α_m=nothing, β_m=nothing, p=nothing, α_h=nothing, β_h=nothing, q=nothing)
-    HHChannel(g, Vsteady, α_m, β_m, p, α_h, β_h, q)
+function hhchannel(identifier, g, Vsteady, α_m=nothing, β_m=nothing, p=nothing, α_h=nothing, β_h=nothing, q=nothing)
+    HHChannel(identifier, g, Vsteady, α_m, β_m, p, α_h, β_h, q)
+end
+
+id(channel::HHChannel) = channel.identifier
+conductance(channel::HHChannel) = channel.g
+alpha_m(channel::HHChannel) = channel.α_m
+beta_m(channel::HHChannel) = channel.β_m
+m_exponent(channel::HHChannel) = channel.p
+alpha_h(channel::HHChannel) = channel.α_h
+beta_h(channel::HHChannel) = channel.β_h
+h_exponent(channel::HHChannel) = channel.q
+
+
+# Struct for holding an entire model
+
+struct HHModel{T<:Number}
+    channels::Vector{HHChannel}
+    I::T
+    C::T
+end
+
+channels(model::HHModel) = model.channels
+
+function buildmodel(model::HHModel)
+    parsed = String[]
+	for (_, ch) in enumerate(channels(model))
+    end
+end
+
+# Parse a single channel
+function parsechannel(channel::HHChannel)
+    "(g$(id(channel)) * (m$(id(channel))^$(m_exponent(channel))) * (h$(id(channel))^$(h_exponent(channel))) * (v-E$(id(channel))))"
 end
 
 end
